8a627064044bca5d27df34291228faf511c8690f
muhammadali <muhammadali@gmail.com>
2025-11-07 17:05:34 -0500

    update welcome file and add loop

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,8 @@
 public class Welcome {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Java + Jenkins + Docker pipeline!");
+    public static void main(String[] args) throws InterruptedException {
+        while(true) {
+            System.out.println("Welcome to Java + Jenkins + Docker pipeline!");
+            Thread.sleep(5000); // Wait 5 seconds between prints
+        }
     }
 }
-
